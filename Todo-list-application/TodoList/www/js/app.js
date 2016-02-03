26603186d13376cafeb57203d753d0eea18d9c28
Rodrigo Schmitz <rodrigo.schmitz@tekoa.com.br>
2016-02-03 13:05:08 -0200

    Escondendo tarefa concluida e mostrando tarefas escondidas

--- Todo-list-application/TodoList/www/js/app.js
+++ Todo-list-application/TodoList/www/js/app.js
@@ -28,9 +28,14 @@ app.controller('mainController', function($scope){
  var tasks = new getTasks();
 
  $scope.lista = tasks.items;
+ $scope.showMarked = false;
 
  $scope.onMarkTask = function(item){
   // console.log("pasosu");
   item.finalizada = !item.finalizada;
+ }
+
+ $scope.onHideItem = function(item){
+  return item.finalizada && !$scope.showMarked
  }
 });
